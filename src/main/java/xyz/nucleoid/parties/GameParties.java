6789319201e625b49a8fdfe98cc5661bf5c95619
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-08-06 15:00:17 -0400

    Update mod to Minecraft 1.19.1 (#9)

--- src/main/java/xyz/nucleoid/parties/GameParties.java
+++ src/main/java/xyz/nucleoid/parties/GameParties.java
@@ -1,12 +1,12 @@
 package xyz.nucleoid.parties;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public final class GameParties implements ModInitializer {
     @Override
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             PartyCommand.register(dispatcher);
         });
 
--- src/main/java/xyz/nucleoid/parties/PartyTexts.java
+++ src/main/java/xyz/nucleoid/parties/PartyTexts.java
@@ -4,7 +4,7 @@ import java.util.UUID;
 
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
-import net.minecraft.text.TranslatableText;
+import net.minecraft.text.Text;
 import xyz.nucleoid.plasmid.game.GameTexts;
 
 public final class PartyTexts {
@@ -14,61 +14,61 @@ public final class PartyTexts {
 
     public static MutableText displayError(PartyError error, String player) {
         return switch (error) {
-            case DOES_NOT_EXIST -> new TranslatableText("text.game_parties.party.error.does_not_exist");
-            case ALREADY_INVITED -> new TranslatableText("text.game_parties.party.error.already_invited", player);
-            case ALREADY_IN_PARTY -> new TranslatableText("text.game_parties.party.error.already_in_party");
-            case CANNOT_REMOVE_SELF -> new TranslatableText("text.game_parties.party.error.cannot_remove_self");
-            case NOT_IN_PARTY -> new TranslatableText("text.game_parties.party.error.not_in_party", player);
-            case NOT_INVITED -> new TranslatableText("text.game_parties.party.error.not_invited");
+            case DOES_NOT_EXIST -> Text.translatable("text.game_parties.party.error.does_not_exist");
+            case ALREADY_INVITED -> Text.translatable("text.game_parties.party.error.already_invited", player);
+            case ALREADY_IN_PARTY -> Text.translatable("text.game_parties.party.error.already_in_party");
+            case CANNOT_REMOVE_SELF -> Text.translatable("text.game_parties.party.error.cannot_remove_self");
+            case NOT_IN_PARTY -> Text.translatable("text.game_parties.party.error.not_in_party", player);
+            case NOT_INVITED -> Text.translatable("text.game_parties.party.error.not_invited");
         };
     }
 
     public static MutableText joinSuccess(ServerPlayerEntity player) {
-        return new TranslatableText("text.game_parties.party.join.success", player.getDisplayName());
+        return Text.translatable("text.game_parties.party.join.success", player.getDisplayName());
     }
 
     public static MutableText leaveSuccess(ServerPlayerEntity player) {
-        return new TranslatableText("text.game_parties.party.leave.success", player.getDisplayName());
+        return Text.translatable("text.game_parties.party.leave.success", player.getDisplayName());
     }
 
     public static MutableText disbandSuccess() {
-        return new TranslatableText("text.game_parties.party.disband.success");
+        return Text.translatable("text.game_parties.party.disband.success");
     }
 
     public static MutableText transferredSender(ServerPlayerEntity transferredTo) {
-        return new TranslatableText("text.game_parties.party.transferred.sender", transferredTo.getDisplayName());
+        return Text.translatable("text.game_parties.party.transferred.sender", transferredTo.getDisplayName());
     }
 
     public static MutableText transferredReceiver(ServerPlayerEntity transferredFrom) {
-        return new TranslatableText("text.game_parties.party.transferred.receiver", transferredFrom.getDisplayName());
+        return Text.translatable("text.game_parties.party.transferred.receiver", transferredFrom.getDisplayName());
     }
 
     public static MutableText kickedSender(ServerPlayerEntity player) {
-        return new TranslatableText("text.game_parties.party.kicked.sender", player.getDisplayName());
+        return Text.translatable("text.game_parties.party.kicked.sender", player.getDisplayName());
     }
 
     public static MutableText kickedReceiver() {
-        return new TranslatableText("text.game_parties.party.kicked.receiver");
+        return Text.translatable("text.game_parties.party.kicked.receiver");
     }
 
     public static MutableText invitedSender(ServerPlayerEntity player) {
-        return new TranslatableText("text.game_parties.party.invited.sender", player.getDisplayName());
+        return Text.translatable("text.game_parties.party.invited.sender", player.getDisplayName());
     }
 
     public static MutableText invitedReceiver(ServerPlayerEntity owner, UUID uuid) {
-        return new TranslatableText("text.game_parties.party.invited.receiver", owner.getDisplayName())
+        return Text.translatable("text.game_parties.party.invited.receiver", owner.getDisplayName())
                 .append(PartyTexts.inviteNotificationLink(owner, uuid));
     }
 
     public static MutableText inviteNotificationLink(ServerPlayerEntity owner, UUID uuid) {
-        return new TranslatableText("text.game_parties.party.invited.receiver.click")
+        return Text.translatable("text.game_parties.party.invited.receiver.click")
                 .setStyle(GameTexts.commandLinkStyle(
                         "/party accept " + uuid,
-                        new TranslatableText("text.game_parties.party.invited.receiver.hover", owner.getDisplayName())
+                        Text.translatable("text.game_parties.party.invited.receiver.hover", owner.getDisplayName())
                 ));
     }
 
     public static MutableText leftGame(ServerPlayerEntity player) {
-        return new TranslatableText("text.game_parties.party.left_game", player.getDisplayName());
+        return Text.translatable("text.game_parties.party.left_game", player.getDisplayName());
     }
 }
